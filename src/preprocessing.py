b90efcadc7b9dc52abd040105d1e1ab09809299b
bhav1497 <104519349+bhav1497@users.noreply.github.com>
2024-05-16 10:33:05 +0200

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,7 +3,7 @@ from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
 from sklearn.impute import SimpleImputer
 
 
-movies_df = pd.read_csv('C:/Users/Bhavana/OneDrive/Desktop/MovieRecommendationSystem/data/movies.csv')
+movies_df = pd.read_csv('data/movies.csv')#C:/Users/Bhavana/OneDrive/Desktop/MovieRecommendationSystem/
 
 movies_df['budget'].fillna(0, inplace=True)
 movies_df['gross'].fillna(0, inplace=True)
@@ -16,6 +16,6 @@ movies_df['country'] = label_encoder.fit_transform(movies_df['country'])
 scaler = StandardScaler()
 movies_df[['budget', 'gross', 'runtime']] = scaler.fit_transform(movies_df[['budget', 'gross', 'runtime']])
 
-preprocessed_file_path = 'C:/Users/Bhavana/OneDrive/Desktop/MovieRecommendationSystem/data/preprocessed_movies.csv'
+preprocessed_file_path = 'data/preprocessed_movies.csv'#C:/Users/Bhavana/OneDrive/Desktop/MovieRecommendationSystem
 movies_df.to_csv(preprocessed_file_path, index=False)
 print("Data preprocessing completed. Preprocessed dataset saved as 'preprocesses_movies.csv'")
